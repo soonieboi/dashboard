9d310cdc1e438cc379d9fa3008591d84af247d83
soonieboi <sherminh0512@gmail.com>
2023-12-22 15:22:33 +0800

    First Commit for Dashboard

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/*!
+
+=========================================================
+* Now UI Dashboard React - v1.5.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/now-ui-dashboard.scss?v1.5.0";
+import "assets/css/demo.css";
+
+import AdminLayout from "layouts/Admin.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/admin/*" element={<AdminLayout />} />
+      <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
+    </Routes>
+  </BrowserRouter>
+);
